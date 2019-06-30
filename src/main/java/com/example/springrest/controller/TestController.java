8874f83672e2b8e2f8770669df479c3c9fcf5aea
im-komo <pravesh.tlabs@gmail.com>
2019-06-30 23:10:02 +0530

    sample controller

---
+++ src/main/java/com/example/springrest/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.example.springrest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "")
+public class TestController {
+	
+	@GetMapping
+	public String test() {
+		return "Hello World!";
+	}
+
+}
